abb2aa53447434578284002e41d407a767c46350
noozan <noozaan@gmail.com>
2024-06-30 20:02:10 +0500

    new

--- read.py
+++ read.py
@@ -5,6 +5,7 @@ import os
 from telegram import Bot
 import asyncio
 from telegram.constants import ParseMode
+from datetime import datetime
 
 # List of game identifiers
 games = [
@@ -31,6 +32,8 @@ results_file = 'game_statuses.json'
 # Telegram bot token and channel ID
 telegram_bot_token = '7320334242:AAE2wIj6HoAcm8pBmdXUCR_5ylcSLDEkbMY'
 telegram_channel_id = '-1002193508333'
+
+
 # Initialize the Telegram bot
 bot = Bot(token=telegram_bot_token)
 
@@ -56,20 +59,36 @@ async def send_telegram_message(game, status, srp):
     processed_text = game.replace('-', ' ').upper()
 
     message = f"<b>{processed_text}</b>\nStatus : {status} {status_icon}\nSRP : {srp}%"
-    await bot.send_message(chat_id=telegram_channel_id, text=message, parse_mode=ParseMode.HTML)
+    sent_message = await bot.send_message(chat_id=telegram_channel_id, text=message, parse_mode=ParseMode.HTML)
+    return sent_message.message_id
+
+# Function to delete message from Telegram channel
+async def delete_telegram_message(message_id):
+    try:
+        await bot.delete_message(chat_id=telegram_channel_id, message_id=message_id)
+    except Exception as e:
+        print(f"Failed to delete message: {e}")
 
 # Function to check and handle changes
 async def check_and_handle_changes(game, status, srp):
     previous_status = previous_results.get(game, {}).get("status")
     previous_srp = previous_results.get(game, {}).get("SRP")
+    previous_message_id = previous_results.get(game, {}).get("message_id")
 
     # Check for changes in status or significant SRP increase when status is Hot
     if status != previous_status or (status == "Hot" and srp and previous_srp and float(srp) > float(previous_srp)):
-        print(f"Sending message for {game}")
-        await send_telegram_message(game, status, srp)
-
-    # Update previous_results with the latest data
-    previous_results[game] = {"status": status, "SRP": srp}
+        print(f"{datetime.now()} - Sending message for {game}")
+        if previous_message_id and status != previous_status:
+            await delete_telegram_message(previous_message_id)
+        new_message_id = await send_telegram_message(game, status, srp)
+        
+        # Update previous_results with the latest data including the message_id
+        previous_results[game] = {
+            "status": status,
+            "SRP": srp,
+            "last_edited": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "message_id": new_message_id
+        }
 
 # Main function to run the scraping and checking
 async def main():
@@ -114,6 +133,9 @@ async def main():
 
         # Check and handle changes
         await check_and_handle_changes(game, game_status, srp_value)
+
+        # Wait for 10 seconds before processing the next game
+        await asyncio.sleep(1)
 
     # Save the updated previous_results to the file
     with open(results_file, 'w') as file:
