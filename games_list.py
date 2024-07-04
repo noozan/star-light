6ef7dc1e776f55d81e56e8b98f265100dfbe4581
noozan <noozaan@gmail.com>
2024-07-04 11:38:30 +0500

    games

---
+++ games_list.py
@@ -0,0 +1,16 @@
+# games_list.py
+
+games = [
+    "Gates-of-Olympus",
+    "Gates-of-Olympus-1000",
+    "Starlight-Princess-1000",
+    "Starlight-Princess",
+    "Sugar-Rush-1000",
+    "Sugar-Rush",
+    "Fire-Portals",
+    "Wanted-Dead-or-a-Wild",
+    "Book-of-Dead",
+    "Sweet-Bonanza",
+    "Big-Bass-Splash",
+    "Big-Bass-Bonanza"
+]
